ae907d6470ea2cfb94ddddd3da657940a44d914c
Ki Chang <kiwchang@gmail.com>
2018-02-24 17:23:57 -0800

    no need to cd into tmp dir within download

--- create-unattended-iso.sh
+++ create-unattended-iso.sh
@@ -31,7 +31,6 @@ download()
 {
     local url=$1
     echo -n "    "
-    cd $tmp
     wget --progress=dot $url 2>&1 | grep --line-buffered "%" | \
         sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
     echo -ne "\b\b\b\b"
